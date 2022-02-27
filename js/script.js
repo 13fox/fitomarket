e6be638f36133e40623b59473a63ee430debda9b
13Fox <foxoedov@gmail.com>
2022-02-27 09:35:23 +0300

    Первая

---
+++ js/script.js
@@ -0,0 +1,142 @@
+$( document ).ready(function() {
+    var swiper = new Swiper(".main-slider", {
+        cssMode: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        mousewheel: true,
+        keyboard: true,
+    });
+  
+    $('.grid').masonry({
+      // options
+      itemSelector: '.grid-item',
+      columnWidth: 80,
+    });
+
+   $('.header-top__list').click(function(){
+    $('.citys_show_ul').toggle() ;
+    $(this).toggleClass('active');
+      })
+$('#serachfield').focus(function(){
+       $('.header-top__search').find('svg').css('stroke','#E3688E')
+})
+
+$('#serachfield').focusout(function(){
+  $('.header-top__search').find('svg').css('stroke','#25C5C2')
+})
+/*$('.product-item').each(function(){
+ let bool=$(this).find('div.product-item__hover')
+  console.log(bool)
+})*/
+
+
+
+if ($(window).width() > 1263)
+  {
+    let wwidth=$(window).width();
+    let percent=(wwidth*10/1263)*1.2+'%';
+    $('.swiper-button-prev').css('left',percent);
+    $('.swiper-button-next').css('right',percent);
+  }
+/*$('.reviews__item:last-child').hide()
+
+$('.reviews__btn').click(function(){
+  $('.reviews__item ').show('slow')
+  return false;
+
+})*/
+
+
+
+
+
+
+    if ($(window).width() < 769) {
+      $('.products').addClass('swiper-wrapper').removeClass('products')
+
+      var swiper2 = new Swiper(".swiper", {
+        cssMode: true,        
+        mousewheel: true,
+        keyboard: true,
+        slidesPerView: 1,
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },    
+        breakpoints: {
+          // when window width is >= 320px
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+          },
+          // when window width is >= 480px
+          480: {
+            slidesPerView: 2,
+            spaceBetween: 30
+          },
+          // when window width is >= 640px
+          640: {
+            slidesPerView: 4,
+            spaceBetween: 40
+          }
+        }
+    });
+      $('.header-mobile__burger').click(function(){
+        $('.header').toggleClass('active')
+        $('.openmenu').toggleClass('active')
+      })
+      $('.open_burger').click(function(){
+        $('.openmenu').toggleClass('active')
+        $('.header').toggleClass('active')
+      })
+
+      $('.zone__item-title').click(function (){
+        $(this).parent().find('.zone__item-description').slideToggle();
+        $(this).parent().toggleClass('active');        
+
+      });
+      $('.header-mobile__search').click(function(){
+        $(this).toggleClass('active');
+        $('.header-top__search').toggleClass('active')
+        })
+
+        $('.menu>li>a').click(function(){
+            let submenu=$(this).parent().find('nav');
+            submenu.slideToggle();
+        })
+    }
+    else
+    {
+      $('.product-item').mouseover(function(){
+        if(!$(this).hasClass('indiv_bouq'))
+        {
+          $(this).find('div.product_inner').addClass('absolute_height')  
+          $(this).addClass('long-container')
+        }  
+      });
+      $('.product-item').mouseout(function(){
+        
+        $(this).find('div.product_inner').removeClass('absolute_height')   
+        $(this).removeClass('long-container')
+      }); 
+
+    }
+    
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+  var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
+  console.log(scrollbar);
+  document.querySelector('[href="#openModal"]').addEventListener('click',function(){
+    document.body.style.overflow = 'hidden';
+    document.querySelector('#openModal').style.marginLeft = scrollbar;
+  });
+  document.querySelector('[href="#close"]').addEventListener('click',function(){
+    document.body.style.overflow = 'visible';
+    document.querySelector('#openModal').style.marginLeft = '0px';
+  });
+});
